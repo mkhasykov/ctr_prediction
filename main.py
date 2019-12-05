6920fdb1954d58d6ed6ed4bcca955a74debc845a
mkhasykov <mkhasykov@gmail.com>
2019-12-05 19:23:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def main():
     Usage: python main.py -f ./data_refactored -o output.json -n 7 -d 18 -alpha .1 -beta 1. -l1 5. -l2 5.
     
     For more information see:
-    - https://git.snatchdev.com/kimberlite/backend/wikis/%D0%92%D0%B5%D1%80%D0%BE%D1%8F%D1%82%D0%BD%D0%BE%D1%81%D1%82%D1%8C-%D0%BA%D0%BE%D0%BD%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D0%B8-%D0%BD%D0%B0-%D1%82%D1%80%D0%B0%D0%BD%D0%B7%D0%B8%D1%82%D0%BA%D0%B5
+    - https://github.com/mkhasykov/for_rtb_pricing_function/blob/master/README.md
     """
     
     parser = ArgumentParser(
